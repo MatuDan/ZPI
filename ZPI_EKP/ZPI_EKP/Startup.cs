27e0ca2a9e7d4e4c61b6739bdd4cd848327d1314
MatuDan <matufishu@gmail.com>
2015-05-01 23:09:52 +0200

    test

---
+++ ZPI_EKP/ZPI_EKP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZPI_EKP.Startup))]
+namespace ZPI_EKP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
